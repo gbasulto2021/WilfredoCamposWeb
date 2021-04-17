93d3a288263aaf18af6da8fefa80d34a937d6fdb
George <gbasulto2015@gmail.com>
2021-04-17 18:56:41 -0400

    Primer commit camposWeb

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const d = document;
+const menu = d.querySelector('.menu');
+const menuBars = d.querySelector('#menu-bars');
+const menuLink = d.querySelectorAll('.menu > li');
+const menuCat = d.querySelector('#menu-cat');
+const items = d.querySelectorAll('.grid_galery .grid_galery__item');
+
+menuBars.addEventListener('click', ()=>{
+    menu.classList.toggle('menu-is-active');
+    menuLink.forEach((list)=>{
+        list.classList.toggle('display');
+    })
+});
+
+menuCat.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let textFilter = e.target.textContent.toLowerCase();
+    items.forEach((item)=> {
+        
+        if(item.textContent.toLowerCase().includes(textFilter)){
+            item.classList.remove('inactivo');
+        }else{
+            item.classList.add('inactivo');
+        }
+    });
+    if(textFilter==='todos'){
+        items.forEach((item)=> {
+            item.classList.remove('inactivo');
+        });
+    }
+});
